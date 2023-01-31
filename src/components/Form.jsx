b2b36556639fc087c79e58289c40f5cfd505d39f
Maksim <polonets.maksim@gmail.com>
2023-01-31 17:53:13 +0300

    add: continue studying

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import MyButton from "./UI/button/MyButton";
+import MyInput from "./UI/input/MyInput";
 
 const Form = () => {
+  const [title, setTitle] = useState("asdf");
+  const [body, setBody] = useState("asdf");
+  const bodyInputRef = useRef();
+  const addNewPost = (e) => {
+    e.preventDefault();
+    const newPost = {
+      id: Date.now(),
+      title,
+      body,
+    };
+    setPosts([...posts, newPost] )
+    console.log(newPost);
+  };
   return (
     <form>
-<input type="text" placeholder="Название поста" />
-      <input type="text" placeholder="Описание поста" />
-      <MyButton disabled = {true}>Push the button</MyButton>
+      <MyInput
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        type="text"
+        placeholder="Название поста"
+      />
+      <MyInput
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        type="text"
+        placeholder="Описание поста"
+      />
+      <MyButton onClick={addNewPost}>Push the button</MyButton>
     </form>
-  )
-}
+  );
+};
 
 export default Form;
